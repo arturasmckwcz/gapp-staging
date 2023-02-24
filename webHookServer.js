e3ab489b879a4b0aa87f772799bd4e86d3ad459e
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-02-24 14:22:12 +0000

    added registering with GitHub

--- webHookServer.js
+++ webHookServer.js
@@ -7,13 +7,78 @@ app.use(express.json());
 
 const buildFolder = process.env.BUILD_FOLDER;
 const buildScript = process.env.BUILD_SCRIPT;
-const url = process.env.WEBHOOK_SERVER_URL;
-const access_token = process.env.WEBHOOK_SERVER_ACCESS_TOKEN;
+const webhookUrl = process.env.WEBHOOK_SERVER_URL;
+const repoOwner = process.env.WEBHOOK_REPO_OWNER;
+const repo = process.env.WEBHOOK_REPO;
+const accessToken = process.env.WEBHOOK_SERVER_ACCESS_TOKEN;
+const webhookSecret = process.env.WEBHOOK_SECRET;
 
-if (!(buildFolder && buildScript && url && access_token))
+let isRegistered = false;
+
+if (
+  !(
+    buildFolder &&
+    buildScript &&
+    webhookUrl &&
+    webhookSecret &&
+    repoOwner &&
+    repo &&
+    accessToken
+  )
+)
   throw new Error('No sufficient data provided.');
 
 // Register with GitHub
+app.post('/register', (req, res) => {
+  const active = req.body.active;
+  if (webhookSecret != req.headers.authorization.substring(7)) {
+    console.log('Unauthorized, ignoring...');
+    return res.end();
+  }
+  if (active && isRegistered) {
+    console.log('Already registered, ignoring...');
+    return res.end();
+  }
+  if (!active && !isRegistered) {
+    console.log('Already unregistered, ignoring...');
+    return res.end();
+  }
+
+  isRegistered = active;
+
+  const repoUrl = `https://api.github.com/repos/${repoOwner}/${repo}/hooks`;
+
+  const webhookContentType = 'json';
+  const webhookEvents = ['push', 'pull_request'];
+
+  fetch(repoUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({
+      name: 'web',
+      active,
+      events: webhookEvents,
+      config: {
+        url: webhookUrl,
+        content_type: webhookContentType,
+        secret: webhookSecret,
+      },
+    }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+  console.log(active ? 'Registered' : 'Unregistered');
+  return res.status(200).end();
+});
 
 app.post('/webhook', (req, res) => {
   if (
@@ -42,7 +107,7 @@ app.post('/webhook', (req, res) => {
 
       res.status(200).send('Build triggered');
     } catch (error) {
-      throw new Error(error.message);
+      console.error(error.message);
     }
   } else {
     res.status(200).send('Notification ignored');
